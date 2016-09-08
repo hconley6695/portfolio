559e3b684bfcd68a82ae997f23ae2cbfc1501957
Heather Conley <hconley6695@gmail.com>
2016-09-08 16:51:02 -0400

    finished jquery in my projects section

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,64 @@ import $ from 'jquery';
 // 		$('#ninja-slides').fadeIn('slow');
 // 	});
 // });
+
+
+$('.milkninja').on('click', function() {
+	$('.milkninja').removeClass('normal');
+	$('.milkninja').addClass('big');
+	$('.restaurant').addClass('normal');
+	$('.game').addClass('normal');
+	$('.wildmild').addClass('normal');
+
+	$('.milkninja-details').removeClass('unseen');
+	$('.milkninja-details').addClass('selected');
+	$('.restaurant-details').addClass('unseen');
+	$('.game-details').addClass('unseen');
+	$('.wildmild-details').addClass('unseen');
+});
+
+
+$('.wildmild').on('click', function() {
+	$('.milkninja').addClass('normal');
+	$('.restaurant').addClass('normal');
+	$('.game').addClass('normal');
+	$('.wildmild').removeClass('normal');
+	$('.wildmild').addClass('big');
+
+	$('.milkninja-details').addClass('unseen');
+	$('.restaurant-details').addClass('unseen');
+	$('.game-details').addClass('unseen');
+	$('.wildmild-details').removeClass('unseen');
+	$('.wildmild-details').addClass('selected');
+
+});
+
+
+$('.restaurant').on('click', function() {
+	$('.milkninja').addClass('normal');
+	$('.restaurant').removeClass('normal');
+	$('.restaurant').addClass('big');
+	$('.game').addClass('normal');
+	$('.wildmild').addClass('normal');
+
+	$('.milkninja-details').addClass('unseen');
+	$('.restaurant-details').removeClass('unseen');
+	$('.restaurant-details').addClass('selected');
+	$('.game-details').addClass('unseen');
+	$('.wildmild-details').addClass('unseen');
+});
+
+$('.game').on('click', function() {
+	$('.milkninja').addClass('normal');
+	$('.restaurant').addClass('normal');
+	$('.game').removeClass('normal');	
+	$('.game').addClass('big');
+	$('.wildmild').addClass('normal');
+
+	$('.milkninja-details').addClass('unseen');
+	$('.restaurant-details').addClass('unseen');
+	$('.game-details').removeClass('unseen');
+	$('.game-details').addClass('selected');
+	$('.wildmild-details').addClass('unseen');
+
+});
