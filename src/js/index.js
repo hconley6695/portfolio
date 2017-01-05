38ae3a440cfe3258b0fc7659f6a136324b6bba2c
Heather Conley <hconley6695@gmail.com>
2017-01-05 16:24:58 -0500

    trying to tie bootstrap into javascript in build tool

--- src/js/index.js
+++ src/js/index.js
@@ -1,63 +1,63 @@
 // Javascript Entry Point
-import $ from 'jquery'; 
+// import jQuery from 'jquery'; 
+import bootstrap from 'bootstrap';
+
+// $('.milkninja').on('click', function() {
+// 	$('.milkninja').removeClass('normal');
+// 	$('.milkninja').addClass('big');
+// 	$('.restaurant').addClass('normal');
+// 	$('.game').addClass('normal');
+// 	$('.wildmild').addClass('normal');
+
+// 	$('.milkninja-details').removeClass('unseen');
+// 	$('.milkninja-details').addClass('selected');
+// 	$('.restaurant-details').addClass('unseen');
+// 	$('.game-details').addClass('unseen');
+// 	$('.wildmild-details').addClass('unseen');
+// });
 
 
-$('.milkninja').on('click', function() {
-	$('.milkninja').removeClass('normal');
-	$('.milkninja').addClass('big');
-	$('.restaurant').addClass('normal');
-	$('.game').addClass('normal');
-	$('.wildmild').addClass('normal');
+// $('.wildmild').on('click', function() {
+// 	$('.milkninja').addClass('normal');
+// 	$('.restaurant').addClass('normal');
+// 	$('.game').addClass('normal');
+// 	$('.wildmild').removeClass('normal');
+// 	$('.wildmild').addClass('big');
 
-	$('.milkninja-details').removeClass('unseen');
-	$('.milkninja-details').addClass('selected');
-	$('.restaurant-details').addClass('unseen');
-	$('.game-details').addClass('unseen');
-	$('.wildmild-details').addClass('unseen');
-});
+// 	$('.milkninja-details').addClass('unseen');
+// 	$('.restaurant-details').addClass('unseen');
+// 	$('.game-details').addClass('unseen');
+// 	$('.wildmild-details').removeClass('unseen');
+// 	$('.wildmild-details').addClass('selected');
 
-
-$('.wildmild').on('click', function() {
-	$('.milkninja').addClass('normal');
-	$('.restaurant').addClass('normal');
-	$('.game').addClass('normal');
-	$('.wildmild').removeClass('normal');
-	$('.wildmild').addClass('big');
-
-	$('.milkninja-details').addClass('unseen');
-	$('.restaurant-details').addClass('unseen');
-	$('.game-details').addClass('unseen');
-	$('.wildmild-details').removeClass('unseen');
-	$('.wildmild-details').addClass('selected');
-
-});
+// });
 
 
-$('.restaurant').on('click', function() {
-	$('.milkninja').addClass('normal');
-	$('.restaurant').removeClass('normal');
-	$('.restaurant').addClass('big');
-	$('.game').addClass('normal');
-	$('.wildmild').addClass('normal');
+// $('.restaurant').on('click', function() {
+// 	$('.milkninja').addClass('normal');
+// 	$('.restaurant').removeClass('normal');
+// 	$('.restaurant').addClass('big');
+// 	$('.game').addClass('normal');
+// 	$('.wildmild').addClass('normal');
 
-	$('.milkninja-details').addClass('unseen');
-	$('.restaurant-details').removeClass('unseen');
-	$('.restaurant-details').addClass('selected');
-	$('.game-details').addClass('unseen');
-	$('.wildmild-details').addClass('unseen');
-});
+// 	$('.milkninja-details').addClass('unseen');
+// 	$('.restaurant-details').removeClass('unseen');
+// 	$('.restaurant-details').addClass('selected');
+// 	$('.game-details').addClass('unseen');
+// 	$('.wildmild-details').addClass('unseen');
+// });
 
-$('.game').on('click', function() {
-	$('.milkninja').addClass('normal');
-	$('.restaurant').addClass('normal');
-	$('.game').removeClass('normal');	
-	$('.game').addClass('big');
-	$('.wildmild').addClass('normal');
+// $('.game').on('click', function() {
+// 	$('.milkninja').addClass('normal');
+// 	$('.restaurant').addClass('normal');
+// 	$('.game').removeClass('normal');	
+// 	$('.game').addClass('big');
+// 	$('.wildmild').addClass('normal');
 
-	$('.milkninja-details').addClass('unseen');
-	$('.restaurant-details').addClass('unseen');
-	$('.game-details').removeClass('unseen');
-	$('.game-details').addClass('selected');
-	$('.wildmild-details').addClass('unseen');
+// 	$('.milkninja-details').addClass('unseen');
+// 	$('.restaurant-details').addClass('unseen');
+// 	$('.game-details').removeClass('unseen');
+// 	$('.game-details').addClass('selected');
+// 	$('.wildmild-details').addClass('unseen');
 
-});
+// });
